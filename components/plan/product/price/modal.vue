49a7869eff90963a3ff8e95ccd49dfa5a523406f
ardiankurniawan8 <40884297+ardiankurniawan8@users.noreply.github.com>
2018-11-12 16:05:11 +0700

    Update harga

---
+++ components/plan/product/price/modal.vue
@@ -0,0 +1,209 @@
+<template>
+  <div>
+    <!-- b-row v-for="(price, index) in defaultHarga" align-v="center">
+      <b-col cols="12" md="12">
+        <span>Harga {{ index + 1 }}</span>
+        <b-link class="text-danger float-right" @click="removePrice(index)"><i class="fa fa-times-circle"></i> Hapus</b-link>
+        <hr class="mt-1 mb-2"/>
+      </b-col>
+      <b-col cols="12" md="11">
+        <b-row>
+          <b-col cols="12" md="6">
+            <b-form-group label="Harga">
+              <money v-model="price.harga" class="text-right form-control" data-vv-name="Harga" v-validate="'required'" :state="errors.has('Harga')"></money>
+              <b-form-invalid-feedback :force-show="errors.has('Harga')">{{ errors.first('Harga') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="6">
+            <b-form-group label="Mulai">
+              <b-form-input data-vv-name="Mulai" v-validate="'required'" v-model="price.mulai" :state="errors.has('Mulai')" type="tel" v-mask="'####/##/## ##:##:##'" masked="true"></b-form-input>
+              <b-form-invalid-feedback :force-show="errors.has('Mulai')">{{ errors.first('Mulai') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" md="3">
+            <b-form-group label="Satuan">
+              <b-form-input data-vv-name="Satuan" v-validate="'required'" v-model="price.penetapan.satuan" :state="errors.has('Satuan')"></b-form-input>
+              <b-form-invalid-feedback :force-show="errors.has('Satuan')">{{ errors.first('Satuan') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="3">
+            <b-form-group label="Bundle">
+              <b-form-input type="number" data-vv-name="Bundle" v-validate="'required'" v-model="price.penetapan.bundle" :state="errors.has('Bundle')" :min="0"></b-form-input>
+              <b-form-invalid-feedback :force-show="errors.has('Bundle')">{{ errors.first('Bundle') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="6">
+            <b-form-group label="Berlaku">
+              <v-select v-model="price.berlaku" :options="['BALINDINOYO','KORAKA']" multiple></v-select>
+              <b-form-invalid-feedback :force-show="errors.has('Berlaku')">{{ errors.first('Berlaku') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-col>
+      <div class="clearfix">&nbsp;</div>
+      <div class="clearfix">&nbsp;</div>
+    </b-row> -->
+
+    <!-- <table class="table">
+      <thead>
+        <tr>
+          <th>Nominal</th>
+          <th>Mulai - Hingga</th>
+          <th>Penetepan <br/>(jumlah/satuan)</th>
+          <th>Berlaku</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(price, index) in defaultHarga">
+          <td><b-form-input type="text" placeholder="" ></b-form-input></td>
+          <td>
+            <b-form-input type="text" placeholder="" ></b-form-input> -
+            <b-form-input type="text" placeholder="" ></b-form-input>
+          </td>
+          <td>
+            <b-form-input type="text" placeholder="" ></b-form-input> /
+            <b-form-input type="text" placeholder="" ></b-form-input>
+          </td>
+          <td>
+            <v-select v-model="price.berlaku" :options="['BALINDINOYO','KORAKA']" multiple></v-select>
+            <b-form-invalid-feedback :force-show="errors.has('Berlaku')">{{ errors.first('Berlaku') }}</b-form-invalid-feedback>
+          </td>
+          <td>
+            <b-link class="text-danger float-right" @click="removeOpsi(index)"><i class="fa fa-times-circle"></i> Hapus</b-link>
+          </td>
+        </tr>
+      </tbody>
+    </table> -->
+    <!-- <b-modal id="modalHarga" title="Tambah Harga Baru" no-close-on-backdrop no-close-on-esc body-class="px-0" ok-title="Simpan" @ok="addHarga" @show="onShow"> -->
+      <b-container class="px-4">
+        <b-row>
+          <b-col cols="12" md="4">
+            <b-form-group label="Currency">
+              <b-form-input data-vv-name="Currency" v-validate="'required|min:3'" :state="errors.has('Currency')" v-model="hargaTemp.harga.currency"></b-form-input>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="8">
+            <b-form-group label="Nominal">
+              <b-form-input data-vv-name="Nominal" v-validate="'required|min:3'" :state="errors.has('Nominal')" v-model="hargaTemp.harga.nominal"></b-form-input>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" md="6">
+            <b-form-group label="Mulai">
+              <b-form-input data-vv-name="Mulai" v-model="hargaTemp.mulai"></b-form-input>
+              <b-form-invalid-feedback :force-show="errors.has('Mulai')">{{ errors.first('Mulai') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="6">
+            <b-form-group label="Hingga">
+              <b-form-input data-vv-name="Hingga" v-model="hargaTemp.hingga"></b-form-input>
+              <b-form-invalid-feedback :force-show="errors.has('Hingga')">{{ errors.first('Hingga') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" md="7">
+            <b-form-group label="Satuan">
+              <b-form-input v-model="hargaTemp.penetapan.satuan"></b-form-input>
+              <b-form-invalid-feedback :force-show="errors.has('Satuan')">{{ errors.first('Satuan') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" md="5">
+            <b-form-group label="Bundle">
+              <b-form-input v-model="hargaTemp.penetapan.bundle"></b-form-input>
+              <b-form-invalid-feedback :force-show="errors.has('Bundle')">{{ errors.first('Bundle') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" md="12">
+            <b-form-group label="Berlaku di toko/cabang">
+              <v-select v-model="hargaTemp.berlaku" :options="['BALINDINOYO','KORAKA']" multiple></v-select>
+              <b-form-invalid-feedback :force-show="errors.has('Berlaku')">{{ errors.first('Berlaku') }}</b-form-invalid-feedback>
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-container>
+    <!-- </b-modal> -->
+    <div class="clearfix">&nbsp;</div>
+    <div slot="modal-footer" class="modal-footer">
+        <b-btn variant="link" @click="hideModal">Cancel</b-btn>
+        <b-btn variant="primary" @click="addHarga">Save Changes</b-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    defaultHarga: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    showModal: {
+      type: Boolean,
+      default: function () {
+        return false
+      }
+    }
+  },
+  data () {
+    return {
+      hargaTemp: {
+        harga: {
+          nominal: '30000',
+          currency: 'IDR'
+        },
+        mulai: '22/11/2018',
+        hingga: '29/11/2018',
+        penetapan: {
+          satuan: 'set',
+          bundle: '1'
+        },
+        berlaku: []
+      },
+      harga: []
+    }
+  },
+  computed: {
+  },
+  methods: {
+    hideModal () {
+      this.$emit('HIDE', true)
+    },
+    onShow (evt) {
+      // this.clearHarga()
+    },
+    addHarga (evt) {
+      if (!this.hargaTemp.harga) {
+      } else {
+        this.harga.push(this.hargaTemp)
+        console.log(this.harga)
+        this.$emit('HARGA', this.hargaTemp)
+      }
+    },
+    removePrice (index) {
+      this.hargaTemp.splice((index - 1), 1)
+    },
+    clearHarga () {
+      this.hargaTemp.harga.nominal = ''
+      this.hargaTemp.harga.currency = ''
+      this.hargaTemp.mulai = ''
+      this.hargaTemp.hingga = ''
+      this.hargaTemp.penetapan.satuan = ''
+      this.hargaTemp.penetapan.bundle = ''
+      this.hargaTemp.berlaku = []
+    }
+  },
+  beforeMount () {
+    console.log('yes sblm mount')
+  }
+}
+</script>
--- pages/basil/plan/_upc.vue
+++ pages/basil/plan/_upc.vue
@@ -5,12 +5,12 @@
         <b-button variant="primary mb-3 px-3" :to="'/basil/plan'">Kembali</b-button>
       </b-col>
     </b-row>
-    <b-card title="Tambah Plan">
+    <b-card title="Edit Plan">
       <div class='card-text pt-4 box-shadow'>
-        <b-form validated> <!-- @submit="onSubmit" --> 
+        <b-form @submit="onSubmit" validated> <!-- @submit="onSubmit" --> 
 
           <div class="row">
-            <p>{{this.table.data.upc}}</p>
+            <!-- <p>{{this.table.data}}</p> -->
             <div class="col-5 col-md-5">
               <b-form-group label="Kode">
                 <b-form-input data-vv-name="kode" v-validate="'required|min:3'" v-model="form.kode" :state="errors.has('kode')" placeholder="Kode"></b-form-input>
@@ -23,19 +23,83 @@
                 <b-form-invalid-feedback :force-show="errors.has('Nama')">{{ errors.first('Nama') }}</b-form-invalid-feedback>
               </b-form-group>
             </div>
-            <div class="col-12 col-md-5">
-              <b-form-group prepend="IDR" label="Harga">
-                <b-form-input min="0" step="1000" data-vv-name="harga" v-validate="'required|min:3'" v-model="form.harga" :state="errors.has('harga')" placeholder="Harga Plan" type="number"></b-form-input>
-                <b-form-invalid-feedback :force-show="errors.has('harga')">{{ errors.first('harga') }}</b-form-invalid-feedback>
-              </b-form-group>
-            </div>
           </div>
+          <p v-if="table.data.list_harga && table.data.list_harga != 0"><strong>List Harga</strong></p>
+          <div v-if="table.data.list_harga && table.data.list_harga != 0" >
+            <b-card  header-tag="header" class="w-100">
+              <b-table hover small
+                show-empty
+                empty-text="Belum ada list harga"
+                class="text-capitalize"
+                :fields="[
+                  {key: 'harga', label: 'Harga', thClass: 'font-weight-bold border-top-0'}, 
+                  {key: 'mulai', label: 'Berlaku Tgl', thClass: 'font-weight-bold border-top-0'}, 
+                  {key: 'penetapan', label: 'Satuan', thClass: 'font-weight-bold border-top-0'}
+                ]"
+                :items="table.data.list_harga">
+                <template slot="harga" slot-scope="data">
+                  <span v-if="data.item.harga">{{ data.item.harga.currency }} {{ data.item.harga.nominal | toCurrency }}</span>
+                </template>
+
+                <template slot="mulai" slot-scope="data">
+                  <span v-if="data.item.mulai">{{ data.item.mulai.date | toDateTime }}</span>
+                </template>
+
+                <template slot="penetapan" slot-scope="data">
+                  <span v-if="data.item.penetapan" class="text-uppercase">{{ data.item.penetapan.satuan }}</span>
+                </template>
+              </b-table>
+            </b-card>
+          </div>
+          <b-card title="Tambah harga baru">
+            <div class="card-text box-shadow">
+             <!--  <b-row v-if="listPrices.length > 0">
+                <b-col cols="12">
+                  <table class="table">
+                    <tr>
+                      <th class="text-dark border-top-0 border-bottom"><strong>Harga</strong></th>
+                      <th class="text-dark border-top-0 border-bottom text-center"><strong>Mulai</strong> - <strong>Berakhir</strong></th>
+                      <th class="text-dark border-top-0 border-bottom text-center"><strong>Satuan (Bundle)</strong></th>
+                      <th class="text-dark border-top-0 border-bottom"><strong>Berlaku</strong></th>
+                      <th class="text-dark border-top-0 border-bottom"></th>
+                    </tr>
+                    <tr v-for="(field, index) in listPrices" class="border-bottom">
+                      <td class="border-0">{{field.harga.currency}} {{field.harga.nominal}}</td>
+                      <td class="border-0 text-center">{{field.mulai}} - {{field.hingga}}</td>
+                      <td class="border-0 text-center">{{field.penetapan.satuan}} ({{field.penetapan.bundle}})</td>
+                      <td class="border-0">
+                        <span v-for="(field2, index2) in field.berlaku">{{field2}}, </span>
+                      </td>
+                      <td class="border-0 text-right" style="width: 15%">
+                        <b-button variant="link" size="sm" class="text-info" @click="removePrice(index)" disabled><i class="fa fa-edit"></i></b-button>
+                        <b-button variant="link" size="sm" class="text-danger" @click="removePrice(index)"><i class="fa fa-times"></i></b-button>
+                      </td>
+                    </tr>
+                  </table>
+                </b-col>
+
+              </b-row> -->
+              <b-row class="pt-3">
+                <b-col cols="12">
+                  <b-link class="text-info" v-b-modal.modalHarga @click="showModalHarga"><i class="fa fa-plus mr-1"></i> Harga Baru</b-link>
+                </b-col>
+              </b-row>
+            </div>
+          </b-card>
+
           <b-row>
-            <b-col cols="12">
+            <b-col cols="6">
               <slot name='action'></slot>
               <b-button type="submit" variant="primary" block :disabled="isLoading">
                 <span v-if="isLoading"><i class="fas fa-spinner fa-spin"></i> Simpan</span>
                 <span v-else>Simpan</span>
+              </b-button>
+            </b-col>
+            <b-col cols="6">
+            <slot name='action'></slot>
+              <b-button type="button" @click="onDelete" variant="danger" block :disabled="isLoading">
+                <span v-if="isLoading"><i class="fas fa-spinner fa-spin"></i>Hapus</span>
+                <span v-else>Hapus</span>
               </b-button>
             </b-col>
           </b-row>
@@ -48,7 +112,8 @@
 </template>
 
 <script>
-import PlanQuery from '~/apollo/queries/query_plan'
+import EditPlanQuery from '~/apollo/queries/query_editplan'
+import EditPlan from '~/apollo/mutations/EditPlan'
 
 export default {
   props: {
@@ -82,8 +147,7 @@ export default {
       },
       form: {
         kode: '',
-        nama: '',
-        harga: ''
+        nama: ''
       }
     }
   },
@@ -101,12 +165,14 @@ export default {
 
       this.$apollo.query(
         {
-          query: PlanQuery,
+          query: EditPlanQuery,
           variables: queryVar,
           fetchPolicy: 'no-cache'
         }
       ).then(function (result) {
         vm.table.data = result.data.pengaturanBarang[0]
+        vm.form.kode = result.data.pengaturanBarang[0].upc
+        vm.form.nama = result.data.pengaturanBarang[0].nama
       }).catch(e => {
         if (e.graphQLErrors && Array.isArray(e.graphQLErrors) && e.graphQLErrors.length) {
           e.graphQLErrors.forEach(function (error) {
@@ -124,6 +190,25 @@ export default {
           vm.$emit('SUBMIT_ERROR', 'Fail to connect to server')
         }
       })
+    },
+    onSubmit (e) {
+      let vm = this
+      e.preventDefault()
+      vm.isLoading = true
+      if (vm.errors.count() === 0) {
+        vm.$apollo.mutate({
+          mutation: EditPlan,
+          variables: vm.form
+        }).then(function (res) {
+          vm.$router.replace('/basil/plan')
+          vm.isLoading = false
+        }).catch(function (e) {
+          vm.$emit('fail', e)
+          vm.isLoading = false
+        })
+      } else {
+        vm.isLoading = false
+      }
     }
   }
 }
