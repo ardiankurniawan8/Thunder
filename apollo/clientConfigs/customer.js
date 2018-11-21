48b524906835197cdea5a92c92a4f18f995cfaa0
Ardian Kurniawan <40884297+ardiankurniawan8@users.noreply.github.com>
2018-11-21 20:44:46 +0700

    Update customer.js

--- apollo/clientConfigs/customer.js
+++ apollo/clientConfigs/customer.js
@@ -8,7 +8,7 @@ export default (ctx) => {
     ctx.redirect('/')
   }
 
-  const httpLink = new BatchHttpLink({ uri: 'http://128.199.145.173:8118/graphql' })
+  const httpLink = new BatchHttpLink({ uri: 'localhost:5000/graphql' })
 
   // middleware
   const middlewareLink = new ApolloLink((operation, forward) => {
